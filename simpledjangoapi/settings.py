a6273b4572c708b7fd2d520df1af7913b4739862
Godspower <godspowereze260@gmail.com>
2021-05-10 19:59:22 +0100

    Did collectstatic

--- simpledjangoapi/settings.py
+++ simpledjangoapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["www.afternoon-basin-61983.herokuapp.com", "afternoon-basin-61983.herokuapp.com"]
 
